c27eda2521999bb99193aa3cef4252107a1adac0
Std1o <37378410+Std1o@users.noreply.github.com>
2020-07-12 19:54:23 +0300

    Initial commit

---
+++ app/src/main/java/com/stdio/cryptotest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.stdio.cryptotest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEncrypt, etDecrypt;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etEncrypt = findViewById(R.id.etEncrypt);
+        etDecrypt = findViewById(R.id.etDecrypt);
+        tv = findViewById(R.id.textView);
+    }
+
+    public void encrypt(View v) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.43.118:5000/encrypt/" + etEncrypt.getText().toString();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject obj;
+                        try {
+                            obj = new JSONObject(response);
+                            tv.setText(obj.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("response " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    public void decrypt(View v) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.43.118:5000/decrypt/" + etDecrypt.getText().toString();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject obj;
+                        try {
+                            obj = new JSONObject(response);
+                            tv.setText(obj.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("response " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
+    }
+}
